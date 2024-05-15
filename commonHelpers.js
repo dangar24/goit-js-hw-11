0a3a6df1a7bed66c250acfd81161817fe2021098
dangar24 <152312857+dangar24@users.noreply.github.com>
2024-05-15 19:56:29 +0000

    Deploying to gh-pages from @ dangar24/goit-js-hw-11@64f5984a8207f1c14f23de0f1846f5c408c7a577 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,33 +1,28 @@
-import{S as m,i as g}from"./assets/vendor-8c59ed88.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))n(t);new MutationObserver(t=>{for(const o of t)if(o.type==="childList")for(const c of o.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&n(c)}).observe(document,{childList:!0,subtree:!0});function r(t){const o={};return t.integrity&&(o.integrity=t.integrity),t.referrerPolicy&&(o.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?o.credentials="include":t.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function n(t){if(t.ep)return;t.ep=!0;const o=r(t);fetch(t.href,o)}})();const i={key:"43845947-8a1e30f8a3261d274f42ac52c",q:"",image_type:"photo",orientation:"horizontal",safesearch:!0},p="https://pixabay.com/api/",f=e=>{let s=`?key=${i.key}&q=${e}&image_type=${i.image_type}&orientation=${i.orientation}&safesearch=${i.safesearch}`;return fetch(`${p}${s}`).then(r=>{if(!r.ok)throw new Error("Запит виконався з помилкою");return r.json()})},h=document.querySelector(".img-list"),y=e=>{const s=e.map(r=>`
+import{S as u,i as d}from"./assets/vendor-8c59ed88.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))c(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const n of s.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&c(n)}).observe(document,{childList:!0,subtree:!0});function t(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerPolicy&&(s.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?s.credentials="include":e.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function c(e){if(e.ep)return;e.ep=!0;const s=t(e);fetch(e.href,s)}})();const i={key:"43845947-8a1e30f8a3261d274f42ac52c",q:"",image_type:"photo",orientation:"horizontal",safesearch:!0},p="https://pixabay.com/api/",g=o=>{let r=`?key=${i.key}&q=${o}&image_type=${i.image_type}&orientation=${i.orientation}&safesearch=${i.safesearch}`;return fetch(`${p}${r}`).then(t=>{if(!t.ok)throw new Error("Запит виконався з помилкою");return t.json()})},f=document.querySelector(".img-list"),h=o=>{const r=o.map(t=>`
                 <li class="img-item">
-                <a class="img-link" href=${r.largeImageURL}>
-                <img src="${r.webformatURL}"
-                alt="${r.tags}"
-                data-source="${r.largeImageURL}" 
+                <a class="img-link" href=${t.largeImageURL}>
+                <img src="${t.webformatURL}"
+                alt="${t.tags}"
+                data-source="${t.largeImageURL}" 
                 class="img">
                 </a>
                 <div class="img-text-box">
                 <div class="img-text">
                 <h4 class="img-text-title">Likes</h4>
-                <p class="img-text-par">${r.likes}</p>
+                <p class="img-text-par">${t.likes}</p>
                 </div>
                 <div class="img-text">
                 <h4 class="img-text-title">Views</h4>
-                <p class="img-text-par">${r.views}</p>
+                <p class="img-text-par">${t.views}</p>
                 </div>
                 <div class="img-text">
                 <h4 class="img-text-title">Comments</h4>
-                <p class="img-text-par">${r.comments}</p>
+                <p class="img-text-par">${t.comments}</p>
                 </div>
                 <div class="img-text">
                 <h4 class="img-text-title">Downloads</h4>
-                <p class="img-text-par">${r.downloads}</p>
+                <p class="img-text-par">${t.downloads}</p>
                 </div>
                 </div>
-                </li>`).join("");h.insertAdjacentHTML("beforeend",s)},x=document.querySelector(".input");document.querySelector(".form-btn");const L=document.querySelector(".box"),u=document.querySelector(".img-list"),l=document.querySelector(".this");let a="";const d=new m,$=e=>{u.innerHTML="",e.currentTarget.reset(),e.preventDefault(),a!==""&&(l.classList.add("loader"),f(a).then(s=>{if(l.classList.remove("loader"),s.hits.length===0){console.log("Sorry, there are no images matching your search query. Please try again!"),g.error({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight",maxWidth:"300px"});return}y(s.hits),d.refresh()}).catch(s=>{console.log(s)}),a="")};L.addEventListener("submit",$);const v=e=>{a=e.currentTarget.value};x.addEventListener("input",v);const S=e=>{e.target!==e.currentTarget&&(d.create(`
-    <img
-      src=${e.target.dataset.source}
-      alt=${e.target.alt}
-    />
-`),instance.show(),e.preventDefault())};u.addEventListener("click",S);
+                </li>`).join("");f.insertAdjacentHTML("beforeend",r)},y=document.querySelector(".input");document.querySelector(".form-btn");const v=document.querySelector(".box"),x=document.querySelector(".img-list"),l=document.querySelector(".this");let a="";const m=new u(".img-list a",{captionsData:"alt",captionsDelay:250,overlayOpacity:0}),L=o=>{x.innerHTML="",o.currentTarget.reset(),o.preventDefault(),a!==""&&(l.classList.add("loader"),g(a).then(r=>{if(l.classList.remove("loader"),r.hits.length===0){console.log("Sorry, there are no images matching your search query. Please try again!"),d.error({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight",maxWidth:"300px"});return}h(r.hits),m.refresh()}).catch(r=>{console.log(r)}),a="")};v.addEventListener("submit",L);const $=o=>{a=o.currentTarget.value};y.addEventListener("input",$);m.refresh();
 //# sourceMappingURL=commonHelpers.js.map
